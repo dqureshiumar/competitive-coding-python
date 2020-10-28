1425f0f79a17d2af790324c86a31cfc215af1ae4
Umar Qureshi <dqureshiumar@gmail.com>
2020-10-28 15:18:00 +0530

    Shuffle the Array Code Added

---
+++ shuffle-the-array.py
@@ -0,0 +1,14 @@
+#Author : Umar Qureshi
+#Leetcode's Shuffle the Array Python3 Solution
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        listi = []
+        i = 0
+        j = n
+        while n:
+            listi.append(nums[i])
+            listi.append(nums[j])
+            i += 1
+            j += 1
+            n -= 1
+        return listi
